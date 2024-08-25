ed4e61c9868217a96af9be2d33af374b33be75e4
benjaminnechicattu <benjaminnechicattu@gmail.com>
2024-08-26 02:07:33 +0530

    added  logger basic

--- app/main.go
+++ app/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+	log "log/slog"
+	"os"
+
+	"github.com/waashy/utils/config/parser"
 )
 
+const CONFIG_FILE_PATH = "../config/runConfig.json"
+
+type ServerConfig struct {
+	Port int `json:"port"`
+}
+
+type AppConfig struct {
+	Server ServerConfig `json:"server_config"`
+}
+
+func init() {}
+
 func main() {
-	fmt.Println("Server Starting")
+	logger := log.New(log.NewJSONHandler(os.Stdout, &log.HandlerOptions{Level: log.LevelDebug}))
+
+	logger.Info("Server Starting")
+
+	var appConfig AppConfig
+
+	err := parser.ConfigParser(CONFIG_FILE_PATH, &appConfig)
+	if err != nil {
+		logger.Error("Failed to parse the configuration", "Err", err)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/waashy/see-user
 
-go 1.20
+go 1.23
+
+require github.com/waashy/utils v0.0.0-20240825193445-87b93e264378
